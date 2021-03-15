5d2ca1878e7b9fd09b0ab89a6ab43cb4c76d3538
AlexandreRCP <alex.rcp@hotmail.com>
2021-03-14 22:10:23 -0300

    Class Usuario criada

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php 
 	require_once("config.php");
 
-	$sql = new Sql();
+/*	$sql = new Sql();
 
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	echo json_encode($usuarios);
+*/
 
+	$cad = new Usuario();
+
+	$cad->loadById(2);
+
+	echo $cad;
 ?>
